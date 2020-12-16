cd80e26e62830d77795b7b191dba40aa3b32597a
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2020-12-16 18:43:50 +0200

    Edited code

--- t09/js/script.js
+++ t09/js/script.js
@@ -1,9 +1,15 @@
 
 function format(date) {
-    return (String(date).length === 1) ? String('0' + date) : date;
+    return (date.toString().length === 1) ? String('0' + date) : date;
 }
 function getFormattedDate(date) {
     let arr = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
         "Friday", "Saturday"];
     return `${format(date.getDate())}.${format(date.getMonth() + 1)}.${date.getFullYear()} ${format(date.getHours())}:${format(date.getMinutes())} ${arr[date.getDay()]}`;
 }
+const date0= new Date(1993, 11, 1);
+const date1= new Date(1998, 0, -33);
+const date2= new Date('42 03:24:00');
+console.log(getFormattedDate(date0));// 01.12.1993 00:00 Wednesday
+console.log(getFormattedDate(date1));// 28.11.1997 00:00 Friday
+console.log(getFormattedDate(date2));// 01.01.2042 03:24 Wednesday
