c422e6d87576847e5d435d7ec32da4767f0c6e80
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2020-12-16 18:47:00 +0200

    commented tests

--- t09/js/script.js
+++ t09/js/script.js
@@ -7,9 +7,9 @@ function getFormattedDate(date) {
         "Friday", "Saturday"];
     return `${format(date.getDate())}.${format(date.getMonth() + 1)}.${date.getFullYear()} ${format(date.getHours())}:${format(date.getMinutes())} ${arr[date.getDay()]}`;
 }
-const date0= new Date(1993, 11, 1);
-const date1= new Date(1998, 0, -33);
-const date2= new Date('42 03:24:00');
-console.log(getFormattedDate(date0));// 01.12.1993 00:00 Wednesday
-console.log(getFormattedDate(date1));// 28.11.1997 00:00 Friday
-console.log(getFormattedDate(date2));// 01.01.2042 03:24 Wednesday
+// const date0= new Date(1993, 11, 1);
+// const date1= new Date(1998, 0, -33);
+// const date2= new Date('42 03:24:00');
+// console.log(getFormattedDate(date0));// 01.12.1993 00:00 Wednesday
+// console.log(getFormattedDate(date1));// 28.11.1997 00:00 Friday
+// console.log(getFormattedDate(date2));// 01.01.2042 03:24 Wednesday
